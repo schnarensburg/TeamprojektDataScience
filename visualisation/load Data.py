6384fb824f36c63d32f3178a8e267f78a8ca7976
Aaron <87277121+schnarensburg@users.noreply.github.com>
2023-05-08 13:20:48 +0200

    Trying to gain a nice csv from the txt input

--- visualisation/load Data.py
+++ visualisation/load Data.py
@@ -15,6 +15,25 @@ if response.status_code == 200:
         for line in file_contents.split('\n'):
             line = line.split(',')
             writer.writerow(line)
-
 else:
     print("Error: could not retrieve file from github")
+
+with open ('daten01.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    lines = list(csv_reader)
+    lines = lines[1:-1]
+with open ('Z13_01.csv', 'w', newline='') as new_csv_file:
+    csv_writer = csv.writer(new_csv_file)
+    csv_writer.writerows(lines)
+
+
+def print_CSV(file):
+    with open(file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        rows = [row for row in csv_reader]
+        for row in rows:
+            row = [element.rstrip() for element in row]
+            print(row)
+
+
+print_CSV('Z13_01.csv')
