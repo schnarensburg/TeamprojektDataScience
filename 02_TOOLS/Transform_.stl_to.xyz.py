75dc7d554653ee66ab8b68602043c01024f1d608
Aaron <87277121+schnarensburg@users.noreply.github.com>
2023-05-16 10:42:08 +0200

    commit16.05.
    new commit

--- 02_TOOLS/Transform_.stl_to.xyz.py
+++ 02_TOOLS/Transform_.stl_to.xyz.py
@@ -1,6 +1,11 @@
 import numpy as np
 from stl import mesh
 
+##<<<<<<< Updated upstream
+##=======
+filepath = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/CAD Files/cutZ19.stl'
+outpath = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/01_DATA/Z19/PointClouds/MeiterradCutZ19.xyz'
+#>>>>>>> Stashed changes
 #Load the STL file and create a mesh object
 stl_file = mesh.Mesh.from_file('input file')
 
--- 04_Visualize/Visualize PointClouds.py
+++ 04_Visualize/Visualize PointClouds.py
@@ -1,9 +1,14 @@
 import numpy as np
 import open3d as o3d
+import os
+
 
 # INPUT 01_DATA / Load data from file
-INPUTdata: object = np.loadtxt(
-    '/00_GENERAL/resources/Kopfwelle_Z13/Transformed data/KW20_Data.xyz')
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "../01_DATA/Z19/PointClouds/MeiterradCutZ19.xyz")
+
+
+INPUTdata: object = np.loadtxt(file_path)
 
 # Convert NumPy array to Open3D PointCloud object
 pcd = o3d.geometry.PointCloud()
@@ -11,3 +16,4 @@ pcd.points = o3d.utility.Vector3dVector(INPUTdata)
 
 # Create Open3D visualization window
 o3d.visualization.draw_geometries([pcd])
+
