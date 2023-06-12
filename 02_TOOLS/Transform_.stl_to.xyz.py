e3d8c0bed1c376aa5df41dc0dec9cc76ea7c834c
Aaron <87277121+schnarensburg@users.noreply.github.com>
2023-06-12 19:15:29 +0200

    Merge remote-tracking branch 'origin/Aaron_N' into Aaron_N
    # Conflicts:
    #	02_TOOLS/Transform_.stl_to.xyz.py
    #	03_Measurement /Measure_Differences_between_same_sized_gears.py

--- 02_TOOLS/Transform_.stl_to.xyz.py
+++ 02_TOOLS/Transform_.stl_to.xyz.py
@@ -3,14 +3,14 @@ from stl import mesh
 
 ##<<<<<<< Updated upstream
 ##=======
-filepath = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/CAD Files/cutZ19.stl'
-outpath = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/01_DATA/Z19/PointClouds/MeiterradCutZ19.xyz'
+filepath = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/01_DATA/Z19/PointClouds/cutZ19.stl'
+outpath = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/01_DATA/Z19/PointClouds/cutCADZ19.xyz'
 #>>>>>>> Stashed changes
 #Load the STL file and create a mesh object
-stl_file = mesh.Mesh.from_file('input file')
+stl_file = mesh.Mesh.from_file(filepath)
 
 #Extract vertices of the mesh as a numpy array
 vertices = stl_file.vectors.reshape((-1, 3))
 
 #Save the vertex coordinates as an XYZ file
-np.savetxt('ouptput file.xyz', vertices, fmt='%f', delimiter=' ')
+np.savetxt(outpath, vertices, fmt='%f', delimiter=' ')
--- 02_TOOLS/Transform_.txt_to_2D.csv.py
+++ 02_TOOLS/Transform_.txt_to_2D.csv.py
@@ -3,33 +3,25 @@ import csv
 
 # Define input and output folder
 dir_in = os.path.dirname(os.path.abspath(__file__))
-input_folder = os.path.join(dir_in, '../01_DATA/Z19/PointClouds/.txt')
+input = os.path.join(dir_in, '../01_DATA/Z19/PointClouds/cutCADZ19.txt')
 
 dir_out = os.path.dirname(os.path.abspath(__file__))
-output_folder = os.path.join(dir_out, '../01_DATA/Z19/PointClouds/.csv2D')
+output = os.path.join(dir_out, '../01_DATA/Z19/PointClouds/2dimZ19CAD.csv')
 
-def reduce_dimension(input_folder, output_folder):
-    for filename in os.listdir(input_folder):
-        if filename.endswith('.txt'):
-            # Read the contents of the input file
-            with open(os.path.join(input_folder, filename), 'r') as file:
-                lines = file.readlines()
-
+def reduce_dimension(input, output):
+        # Read the contents of the input file
+    with open(input, 'r') as file:
+        lines = file.readlines()
             # Remove first and last line and the y-coordinate
-            selected_lines = lines[1:-1]
-            points = [[float(x) for x in line.split()] for line in selected_lines]
-            points = remove_y(points)
-
-            output_filename = os.path.splitext(filename)[0] + '_2D.csv'
-            output_path = os.path.join(output_folder, output_filename)
-
-            # Write points to a CSV
-            with open(output_path, 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerows(points)
+        points = [[float(x) for x in line.split()] for line in lines]
+        points = remove_y(points)
+        # Write points to a CSV
+        with open(output, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(points)
     print('Reduction completed successfully')
 
 def remove_y(points):
-    return[[point[0], point[1]] for point in points]
-reduce_dimension(input_folder, output_folder)
+    return[[point[0], point[2]] for point in points]
+reduce_dimension(input, output)
 
