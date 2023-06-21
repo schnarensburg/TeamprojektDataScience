76e255efc7b9de4a0d27700205ad64fb299004cf
Aaron <87277121+schnarensburg@users.noreply.github.com>
2023-06-21 11:25:21 +0200

    Merge remote-tracking branch 'origin/Aaron_G' into AaronG
    # Conflicts:
    #	02_TOOLS/Transform_.stl_to.xyz.py
    #	03_Measurement /Measure_Differences_between_same_sized_gears.py

--- 03_Measurement /2Dimensional/Variance.py
+++ 03_Measurement /2Dimensional/Variance.py
@@ -1,6 +1,7 @@
 import math
 import os
 import numpy as np
+import csv
 
 
 def calculate_means(txt2D_folder, z13CAD):
@@ -10,7 +11,7 @@ def calculate_means(txt2D_folder, z13CAD):
     for file in file_list:
         file_name = os.path.join(txt2D_folder, file)
 
-        sum = 0
+        sum_mean = 0
         j = 0
         for tooth in range(1, 13):
             j += 1
@@ -19,12 +20,37 @@ def calculate_means(txt2D_folder, z13CAD):
             for point1, point2 in zip(tooth_pc, tooth_CAD):
                 x1, y1 = point1
                 x2, y2 = point2
-                sum += math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                sum_mean += math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-        mean = sum / 13
+        mean = sum_mean / 13
         means_array.append([file, mean])
 
-    print(means_array)
+    print(f" Means: {means_array}")
+    return means_array
+
+
+def calculate_vars(txt2D_folder, z13CAD, means_array):
+    vars_array = []
+    file_list = os.listdir(txt2D_folder)
+
+    for file in file_list:
+        file_name = os.path.join(txt2D_folder, file)
+
+        sum_var = 0
+        j = 0
+        for tooth in range(1, 13):
+            j += 1
+            tooth_pc = get_current_tooth(file_name, j)
+            tooth_CAD = get_current_tooth(z13CAD, j)
+            for point1, point2 in zip(tooth_pc, tooth_CAD):
+                x1, y1 = point1
+                x2, y2 = point2
+                sum_var += (float(means_array[j][1]) - (math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))) ** 2
+        variance = sum_var / 13
+        vars_array.append([file, variance])
+
+    print(f" Variances: {vars_array}")
+    return vars_array
 
 def get_current_tooth(file, j):
     corridor_angle = ((j+0.25) * 360/13, (j+1.25)*360/13)
@@ -45,6 +71,28 @@ def get_current_tooth(file, j):
                 points.append([x, y])
     return np.array(points)
 
+def sort_and_write_to_csv(means, variances, output_file_means, output_file_variances):
+    means_sorted = sorted(means, key=lambda x: x[1], reverse=True)
+    variances_sorted = sorted(variances, key=lambda x: x[1], reverse=True)
+
+    with open(output_file_means, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Filename', 'Mean'])
+
+        for mean in means_sorted:
+            writer.writerow([mean[0], mean[1]])
+
+    with open(output_file_variances, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Filename', 'Variance'])
+
+        for var in variances_sorted:
+            writer.writerow([var[0], var[1]])
+
 txt2D_folder = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/01_DATA/Z13/PointClouds/.txt2D'
 Z13CAD = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/01_DATA/Z13/PointClouds/2dimZ13CAD.txt'
-calculate_means(txt2D_folder, Z13CAD)
+means = calculate_means(txt2D_folder, Z13CAD)
+variances = calculate_vars(txt2D_folder, Z13CAD, means)
+output_file_means = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/10_RESULTS/2D_Analysis/means.csv'
+output_file_variances = '/Users/aarongrommes/Library/CloudStorage/OneDrive-Persönlich/Studies/Semester 6/Teamprojekt/TeamprojektDataScience/TeamprojektDataScience/10_RESULTS/2D_Analysis/variances.csv'
+sort_and_write_to_csv(means, variances, output_file_means, output_file_variances)
